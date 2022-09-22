746d3b663b07f382eff4cff27bd48309ca48aaf6
Alexander Hübener <alex.teamplayer@gmail.com>
2022-09-22 21:25:49 +0200

    refactored

--- postcard-bindgen/examples/generate_bindings.rs
+++ postcard-bindgen/examples/generate_bindings.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 use postcard_bindgen::{export_js_bindings, ArchPointerLen, JsExportable, PostcardBindings};
 use serde_derive::Serialize;
@@ -21,9 +21,16 @@ struct OtherTest {
     alloc_array: alloc::vec::Vec<u32>,
 }
 
+fn export_path() -> PathBuf {
+    let mut exec_path = std::env::current_exe().unwrap();
+    exec_path.pop();
+    exec_path.push("js_export.js");
+    exec_path
+}
+
 fn main() {
     export_js_bindings(
-        Path::new("./js_export.js"),
+        export_path().as_path(),
         vec![Test::js_bindings(), OtherTest::js_bindings()],
         ArchPointerLen::U32,
     )
