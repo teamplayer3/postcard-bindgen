77261eec2e672e6fb6ad070d6b32e6dc72aeacbc
Alexander Hübener <alex.teamplayer@gmail.com>
2022-10-03 17:36:04 +0200

    refactoring

--- postcard-bindgen-core/src/code_gen/type_checking.rs
+++ postcard-bindgen-core/src/code_gen/type_checking.rs
@@ -1,83 +1,108 @@
-use genco::{lang::js::Tokens, quote};
+use genco::{
+    lang::js::Tokens,
+    prelude::JavaScript,
+    quote, quote_in,
+    tokens::{quoted, FormatInto},
+};
+
+use crate::{
+    registry::StructField,
+    type_info::{JsType, ObjectMeta},
+    StringExt,
+};
+
+enum FieldAccess<'a> {
+    Object(&'a str),
+    Array(usize),
+}
+
+impl FormatInto<JavaScript> for FieldAccess<'_> {
+    fn format_into(self, tokens: &mut genco::Tokens<JavaScript>) {
+        quote_in! { *tokens =>
+            $(match self {
+                FieldAccess::Array(i) => [$i],
+                FieldAccess::Object(n) => .$n
+            })
+        }
+    }
+}
+
+fn gen_struct_field_available_checks(fields: impl AsRef<[StructField]>) -> Tokens {
+    and_chain(
+        fields
+            .as_ref()
+            .iter()
+            .map(|field| quote!( $(quoted(&field.name)) in v)),
+    )
+}
+
+fn gen_struct_field_type_checks(fields: impl AsRef<[StructField]>) -> Tokens {
+    and_chain(
+        fields
+            .as_ref()
+            .iter()
+            .map(|field| gen_field_type_check(FieldAccess::Object(&field.name), &field.js_type)),
+    )
+}
+
+fn gen_array_field_type_checks(fields: impl AsRef<[JsType]>) -> Tokens {
+    and_chain(
+        fields
+            .as_ref()
+            .iter()
+            .enumerate()
+            .map(|(index, field)| gen_field_type_check(FieldAccess::Array(index), field)),
+    )
+}
+
+fn gen_field_type_check(field_access: FieldAccess, ty: &JsType) -> Tokens {
+    match ty {
+        JsType::Array(_) => quote!(Array.isArray(v$field_access)),
+        JsType::Object(ObjectMeta { name }) => {
+            quote!(is_$(name.to_obj_identifier())(v$field_access))
+        }
+        _ => quote!(typeof v$field_access === $(quoted(ty.to_string()))),
+    }
+}
 
 pub mod strukt {
-    use genco::{lang::js::Tokens, quote, tokens::quoted};
+    use genco::{lang::js::Tokens, quote};
 
-    use crate::{
-        registry::StructField,
-        type_info::{JsType, ObjectMeta},
-        StrExt, StringExt,
-    };
+    use crate::{registry::StructField, StrExt};
+
+    use super::{gen_struct_field_available_checks, gen_struct_field_type_checks};
 
     pub fn gen_check_func(obj_name: impl AsRef<str>, fields: impl AsRef<[StructField]>) -> Tokens {
         let obj_name = obj_name.as_ref();
 
+        let field_available_checks = gen_struct_field_available_checks(&fields);
+        let field_type_checks = gen_struct_field_type_checks(&fields);
+
         quote! {
             const is_$(obj_name.to_obj_identifier()) = (v) => {
-                return typeof v === "object" && $(gen_field_checks(&fields).iter().chain(&gen_type_checks(fields)).map(|q| q.to_string().unwrap()).collect::<Vec<_>>().join("&&"))
+                return typeof v === "object" && $field_available_checks && $field_type_checks
             }
-        }
-    }
-
-    fn gen_field_checks(fields: impl AsRef<[StructField]>) -> Vec<Tokens> {
-        fields
-            .as_ref()
-            .iter()
-            .map(|field| quote!( $(quoted(&field.name)) in v))
-            .collect::<Vec<_>>()
-    }
-
-    fn gen_type_checks(fields: impl AsRef<[StructField]>) -> Vec<Tokens> {
-        fields
-            .as_ref()
-            .iter()
-            .map(gen_type_check)
-            .collect::<Vec<_>>()
-    }
-
-    fn gen_type_check(field: &StructField) -> Tokens {
-        match &field.js_type {
-            JsType::Array(_) => quote!(Array.isArray(v.$(&field.name))),
-            JsType::Object(ObjectMeta { name }) => {
-                quote!(is_$(name.to_obj_identifier())(v.$(&field.name)))
-            }
-            _ => quote!(typeof v.$(field.name.as_str()) === $(quoted(field.js_type.to_string()))),
         }
     }
 }
 
 pub mod tuple_struct {
-    use convert_case::{Case, Casing};
-    use genco::{lang::js::Tokens, quote, tokens::quoted};
+    use genco::{lang::js::Tokens, quote};
 
-    use crate::type_info::{JsType, ObjectMeta};
+    use crate::{type_info::JsType, StrExt};
+
+    use super::gen_array_field_type_checks;
 
     pub fn gen_check_func(obj_name: impl AsRef<str>, fields: impl AsRef<[JsType]>) -> Tokens {
         let obj_name = obj_name.as_ref();
 
+        let field_count = fields.as_ref().len();
+        let field_type_checks = gen_array_field_type_checks(fields);
+
         quote! {
-            const is_$(obj_name.to_case(Case::Snake).to_uppercase()) = (v) => {
-                return Array.isArray(v) && v.length === $(fields.as_ref().len()) && $(gen_type_checks(fields).iter().map(|q| q.to_string().unwrap()).collect::<Vec<_>>().join("&&"))
+            const is_$(obj_name.to_obj_identifier()) = (v) => {
+                return Array.isArray(v) && v.length === $field_count && $field_type_checks
             }
-        }
-    }
-
-    fn gen_type_checks(fields: impl AsRef<[JsType]>) -> Vec<Tokens> {
-        fields
-            .as_ref()
-            .iter()
-            .enumerate()
-            .map(|(index, field)| gen_type_check(index, field))
-            .collect::<Vec<_>>()
-    }
-
-    fn gen_type_check(index: usize, field: &JsType) -> Tokens {
-        match field {
-            JsType::Array(_) => quote!(Array.isArray(v[$index])),
-            JsType::Object(ObjectMeta { name }) => {
-                quote!(is_$(name.to_case(Case::Snake).to_uppercase())(v[$index]))
-            }
-            _ => quote!(typeof v[$index] === $(quoted(field.to_string()))),
         }
     }
 }
@@ -102,17 +127,20 @@ pub mod enum_ty {
     use genco::{lang::js::Tokens, quote, tokens::quoted};
 
     use crate::{
-        registry::{EnumVariant, EnumVariantType, StructField},
-        type_info::{JsType, ObjectMeta},
-        StrExt, StringExt,
+        registry::{EnumVariant, EnumVariantType},
+        StrExt,
     };
 
-    use super::{and_chain, or_chain};
+    use super::{
+        and_chain, gen_array_field_type_checks, gen_struct_field_available_checks,
+        gen_struct_field_type_checks, or_chain,
+    };
 
     pub fn gen_check_func(
         obj_name: impl AsRef<str>,
         variants: impl AsRef<[EnumVariant]>,
     ) -> Tokens {
+        let obj_name = obj_name.as_ref();
         let enumerated_variants = variants.as_ref().iter().enumerate();
         let simple_variants = enumerated_variants
             .to_owned()
@@ -124,7 +152,7 @@ pub mod enum_ty {
         let simple_variant_checks = gen_simple_type_checks(simple_variants);
         let complex_variant_checks = gen_complex_type_checks(complex_variants);
         quote!(
-            const is_$(obj_name.as_ref().to_obj_identifier()) = (v) => {
+            const is_$(obj_name.to_obj_identifier()) = (v) => {
                 return $simple_variant_checks || $complex_variant_checks
             }
         )
@@ -162,58 +190,13 @@ pub mod enum_ty {
         match &variant.inner_type {
             EnumVariantType::Empty => unreachable!(),
             EnumVariantType::NewType(fields) => {
-                let field_checks = and_chain(gen_field_checks(fields));
-                let type_checks = and_chain(gen_type_checks(fields));
+                let field_checks = gen_struct_field_available_checks(fields);
+                let type_checks = gen_struct_field_type_checks(fields);
                 quote!($field_checks && $type_checks)
             }
             EnumVariantType::Tuple(fields) => {
-                let type_checks = and_chain(
-                    fields
-                        .iter()
-                        .enumerate()
-                        .map(|(index, field)| gen_type_check_array(index, field)),
-                );
+                let type_checks = gen_array_field_type_checks(fields);
                 quote!(Array.isArray(v.inner) && v.inner.length === $(fields.len()) && $type_checks)
-            }
-        }
-    }
-
-    fn gen_field_checks(fields: impl AsRef<[StructField]>) -> Vec<Tokens> {
-        fields
-            .as_ref()
-            .iter()
-            .map(|field| quote!( $(quoted(&field.name)) in v.inner))
-            .collect::<Vec<_>>()
-    }
-
-    fn gen_type_checks(fields: impl AsRef<[StructField]>) -> Vec<Tokens> {
-        fields
-            .as_ref()
-            .iter()
-            .map(gen_type_check)
-            .collect::<Vec<_>>()
-    }
-
-    fn gen_type_check(field: &StructField) -> Tokens {
-        match &field.js_type {
-            JsType::Array(_) => quote!(Array.isArray(v.$(&field.name))),
-            JsType::Object(ObjectMeta { name }) => {
-                quote!(is_$(name.to_obj_identifier())(v.$(&field.name)))
-            }
-            _ => {
-                quote!(typeof v.inner.$(field.name.as_str()) === $(quoted(field.js_type.to_string())))
-            }
-        }
-    }
-
-    fn gen_type_check_array(index: usize, field: &JsType) -> Tokens {
-        match &field {
-            JsType::Array(_) => quote!(Array.isArray(v[$index])),
-            JsType::Object(ObjectMeta { name }) => {
-                quote!(is_$(name.to_obj_identifier())(v[$index]))
-            }
-            _ => {
-                quote!(typeof v.inner[$index] === $(quoted(field.to_string())))
             }
         }
     }
