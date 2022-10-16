91105cfe48adce9aab3a4456beea858a17ac15ea
Alexander Hübener <alex.teamplayer@gmail.com>
2022-10-16 16:25:13 +0200

    added number types

--- postcard-bindgen-core/src/code_gen/type_checking/ts.rs
+++ postcard-bindgen-core/src/code_gen/type_checking/ts.rs
@@ -7,9 +7,29 @@ use crate::{
 
 pub fn gen_ts_typings(bindings: impl AsRef<[BindingType]>) -> Tokens {
     quote!(
+        $(gen_number_decls())
+
         $(gen_type_decl(&bindings))
         $(gen_value_type_decl(bindings))
         $(gen_ser_des_decls())
+    )
+}
+
+fn gen_number_decls() -> Tokens {
+    quote!(
+        declare type u8 = number
+        declare type u16 = number
+        declare type u32 = number
+        declare type u64 = number
+        declare type u128 = number
+        declare type usize = number
+
+        declare type i8 = number
+        declare type i16 = number
+        declare type i32 = number
+        declare type i64 = number
+        declare type i128 = number
+        declare type isize = number
     )
 }
 
