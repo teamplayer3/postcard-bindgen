6b38ea6d0bc8ff2185a021f31e62703e9fde11f9
Alexander Hübener <alex.teamplayer@gmail.com>
2022-11-27 00:06:33 +0100

    refactored

--- postcard-bindgen-derive/src/derive_struct.rs
+++ postcard-bindgen-derive/src/derive_struct.rs
@@ -2,51 +2,55 @@ use proc_macro2::{Ident, TokenStream};
 use quote::quote;
 use serde_derive_internals::ast::{Field, Style};
 
-use super::PRIVATE_IMPORT_PATH;
-
 pub fn derive_struct(style: Style, ident: Ident, fields: Vec<Field>) -> TokenStream {
     let fields = fields
         .into_iter()
         .filter(|field| !(field.attrs.skip_serializing() || field.attrs.skip_deserializing()))
         .collect::<Vec<_>>();
+    let struct_name = ident.to_string();
+    derive_struct_style(style, struct_name, fields)
+}
+
+fn derive_struct_style<'a>(
+    style: Style,
+    struct_name: String,
+    fields: impl AsRef<[Field<'a>]>,
+) -> TokenStream {
     match style {
-        Style::Struct => derive_struct_type(ident, fields),
-        Style::Tuple => derive_tuple_struct_type(ident, fields),
-        Style::Unit => derive_unit_struct_type(ident),
-        Style::Newtype => derive_tuple_struct_type(ident, fields),
+        Style::Struct => derive_struct_type(struct_name, fields),
+        Style::Tuple => derive_tuple_struct_type(struct_name, fields),
+        Style::Unit => derive_unit_struct_type(struct_name),
+        Style::Newtype => derive_tuple_struct_type(struct_name, fields),
     }
 }
 
-fn derive_unit_struct_type(ident: Ident) -> TokenStream {
-    let ident_str = ident.to_string();
+fn derive_unit_struct_type(name: String) -> TokenStream {
     quote!(
-        let mut ty = #PRIVATE_IMPORT_PATH::UnitStructType::new(#ident_str.into());
+        let mut ty = _pb::__private::UnitStructType::new(#name.into());
         reg.register_unit_struct_binding(ty);
     )
 }
 
-fn derive_tuple_struct_type(ident: Ident, fields: Vec<Field>) -> TokenStream {
-    let ident_str = ident.to_string();
-    let body = fields.iter().map(|field| {
+fn derive_tuple_struct_type<'a>(name: String, fields: impl AsRef<[Field<'a>]>) -> TokenStream {
+    let body = fields.as_ref().iter().map(|field| {
         let ty = field.ty;
         quote!(ty.register_field::<#ty>())
     });
     quote!(
-        let mut ty = #PRIVATE_IMPORT_PATH::TupleStructType::new(#ident_str.into());
+        let mut ty = _pb::__private::TupleStructType::new(#name.into());
         #(#body);*;
         reg.register_tuple_struct_binding(ty);
     )
 }
 
-fn derive_struct_type(ident: Ident, fields: Vec<Field>) -> TokenStream {
-    let ident_str = ident.to_string();
-    let body = fields.iter().map(|field| {
+fn derive_struct_type<'a>(name: String, fields: impl AsRef<[Field<'a>]>) -> TokenStream {
+    let body = fields.as_ref().iter().map(|field| {
         let ident_str = field.attrs.name().serialize_name();
         let ty = field.ty;
         quote!(ty.register_field::<#ty>(#ident_str.into()))
     });
     quote!(
-        let mut ty = #PRIVATE_IMPORT_PATH::StructType::new(#ident_str.into());
+        let mut ty = _pb::__private::StructType::new(#name.into());
         #(#body);*;
         reg.register_struct_binding(ty);
     )
