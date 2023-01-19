b7b81bab9cb7663e88970199a8e541a42877002d
Alexander Hübener <alex.teamplayer@gmail.com>
2023-01-19 16:28:54 +0100

    rename output package

--- examples/no_std/gen-bindings/src/main.rs
+++ examples/no_std/gen-bindings/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     build_npm_package(
         std::env::current_dir().unwrap().as_path(),
         PackageInfo {
-            name: "test".into(),
+            name: "no_std_bindings".into(),
             version: "0.1.0".try_into().unwrap(),
         },
         generate_bindings!(no_std::Protocol, no_std::Packet, no_std::A1Meta),
