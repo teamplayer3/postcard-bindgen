f9ef06230418b7b9e543d1e7445fd27a91b65ee5
Alexander Hübener <alex.teamplayer@gmail.com>
2024-10-02 12:15:35 +0200

    impl JsTypeGenerateable for TupleMeta

--- postcard-bindgen-core/src/code_gen/generateable/types/tuple.rs
+++ postcard-bindgen-core/src/code_gen/generateable/types/tuple.rs
@@ -1,7 +1,10 @@
 use genco::{lang::js::Tokens, quote};
 
 use crate::{
-    code_gen::{generateable::VariablePath, JS_OBJECT_VARIABLE},
+    code_gen::{
+        generateable::{types::des::FieldAccessor, VariableAccess, VariablePath},
+        utils::{comma_chain, semicolon_chain},
+    },
     type_info::TupleMeta,
 };
 
@@ -9,19 +12,37 @@ use super::{des, JsTypeGenerateable};
 
 impl JsTypeGenerateable for TupleMeta {
     fn gen_ser_accessor(&self, variable_path: VariablePath) -> Tokens {
-        unimplemented!()
+        semicolon_chain(self.items_types.iter().enumerate().map(|(i, v)| {
+            v.gen_ser_accessor(
+                variable_path
+                    .clone()
+                    .modify_push(VariableAccess::Indexed(i)),
+            )
+        }))
     }
 
     fn gen_des_accessor(&self, field_accessor: des::FieldAccessor) -> Tokens {
-        unimplemented!()
+        let inner_type_accessors = comma_chain(
+            self.items_types
+                .iter()
+                .map(|v| v.gen_des_accessor(FieldAccessor::None)),
+        );
+        quote!($field_accessor[$inner_type_accessors])
     }
 
     fn gen_ty_check(&self, variable_path: VariablePath) -> Tokens {
-        quote!(Array.isArray($variable_path))
+        let type_checks = comma_chain(self.items_types.iter().enumerate().map(|(i, v)| {
+            v.gen_ty_check(
+                variable_path
+                    .clone()
+                    .modify_push(VariableAccess::Indexed(i)),
+            )
+        }));
+        quote!(Array.isArray($(variable_path.clone())) && $variable_path.length === $(self.items_types.len()) && $type_checks)
     }
 
     fn gen_ts_type(&self) -> Tokens {
-        // quote!([$(self.items_types.gen_ts_type()),*])
-        unimplemented!()
+        let type_checks = comma_chain(self.items_types.iter().map(|v| v.gen_ts_type()));
+        quote!([$type_checks])
     }
 }
