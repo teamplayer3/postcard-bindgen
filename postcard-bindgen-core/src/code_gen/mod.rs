a15b1ed7074dd996576dcca4a2173b68099ee953
Alexander Hübener <alexander.huebener@it-designers.de>
2024-10-04 16:12:42 +0200

    switchable between js and python

--- postcard-bindgen-core/src/code_gen/mod.rs
+++ postcard-bindgen-core/src/code_gen/mod.rs
@@ -1,5 +1,11 @@
-
+#[cfg(feature = "js")]
 mod js;
+
+#[cfg(feature = "python")]
+mod python;
 
 #[cfg(feature = "js")]
 pub use js::{generate_js as generate, type_checking::ts::gen_ts_typings as generate_typings};
+
+#[cfg(feature = "python")]
+pub use python::{generate, type_checking::typing::generate_typings};
