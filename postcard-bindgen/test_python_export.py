3f746d1d9cc2a1da2a838a1c81cb87affcb749b8
Alexander Hübener <alexander.huebener@it-designers.de>
2024-10-14 14:21:06 +0200

    fix test

--- postcard-bindgen/test_python_export.py
+++ postcard-bindgen/test_python_export.py
@@ -1,6 +1,6 @@
 from py_test_bindings import *
 
-d_own = D(a=23, b = C_B(32), c=A(), d= [43, 34, 23], e=None, f= [34, 23, 12], g="feerfef", h = range(3, 23), i= {"df": 434}, j={3: 23}, k=[23, 23, 12, 12, 1, 1, 1, 9, 1, 10], m=(32, "def", [32, 232]))
+d_own = D(a=23, b = C_B(32), c=A(), d= [43, 34, 23], e=None, f= [34, 23, 12], g="feerfef", h = range(3, 23), i= {"df": 434}, j={3: 23}, k=[23, 23, 12, 12, 1, 1, 1, 9, 1, 10], m=(32, "def", [32, 232]), n = True)
 
 ser = open("serialized.bytes", "rb").read()
 d = deserialize(D, ser)
