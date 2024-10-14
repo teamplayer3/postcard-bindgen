43b0840cb016cac25ce8ea4f65f32e7ffb80d41f
Alexander Hübener <alexander.huebener@it-designers.de>
2024-10-14 13:42:55 +0200

    fix example

--- examples/no_std/gen-bindings/src/main.rs
+++ examples/no_std/gen-bindings/src/main.rs
@@ -1,12 +1,17 @@
-use postcard_bindgen::{build_npm_package, generate_bindings, PackageInfo};
+use postcard_bindgen::{
+    generate_bindings,
+    javascript::{build_package, GenerationSettings},
+    PackageInfo,
+};
 
 fn main() {
-    build_npm_package(
+    build_package(
         std::env::current_dir().unwrap().as_path(),
         PackageInfo {
             name: "no_std_bindings".into(),
             version: "0.1.0".try_into().unwrap(),
         },
+        GenerationSettings::enable_all(),
         generate_bindings!(no_std::Protocol, no_std::Packet, no_std::A1Meta),
     )
     .unwrap();
