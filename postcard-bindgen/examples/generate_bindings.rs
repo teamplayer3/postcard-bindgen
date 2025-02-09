d25e8f38bba8b796817edc2b0f329fc75c63a5d4
Alexander Hübener <alex.teamplayer@gmail.com>
2025-02-09 22:36:39 +0100

    test: more testing on all types for js and python

--- postcard-bindgen/examples/generate_bindings.rs
+++ postcard-bindgen/examples/generate_bindings.rs
@@ -1,54 +1,115 @@
-use std::{collections::HashMap, io::Write, num::NonZero, ops::Range};
+use std::{collections::HashMap, io::Write, num::NonZero, ops::Range, str::FromStr, u8};
 
 use postcard_bindgen::{generate_bindings, javascript, python, PackageInfo, PostcardBindings};
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
-#[derive(Debug, Serialize, Deserialize, PostcardBindings)]
-struct A;
+#[derive(Debug, Serialize, PostcardBindings)]
+struct UnionContainer;
 
-#[derive(Debug, Serialize, Deserialize, PostcardBindings)]
-struct B(u8, Vec<u16>, String, HashMap<u16, u8>);
-
-#[derive(Debug, Serialize, Deserialize, PostcardBindings)]
-#[allow(dead_code)]
-enum C {
-    A,
-    B(u8),
-    C(A, B),
-    D { a: Vec<u8>, b: B, c: bool },
+#[derive(Debug, Serialize, PostcardBindings)]
+struct StructContainer {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
 }
 
-#[derive(Debug, Serialize, Deserialize, PostcardBindings)]
-struct D {
-    a: u8,
-    b: C,
-    c: A,
-    d: Vec<u8>,
-    e: Option<u8>,
-    f: &'static [u8],
-    g: &'static str,
-    h: Range<u16>,
-    i: HashMap<String, u16>,
-    j: HashMap<u16, u8>,
-    k: [u8; 10],
-    l: e::E,
-    m: (u8, String, Vec<u8>),
-    n: bool,
-    o: NonZero<u32>,
-    p: i8,
-    q: i16,
+#[derive(Debug, Serialize, PostcardBindings)]
+enum EnumContainer {
+    A,
+    B(u8),
+    C(u8, StructContainer),
+    D { a: u8, b: StructContainer },
+}
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct TupleContainer(u8, StructContainer, EnumContainer);
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct ContainerTypes {
+    u: UnionContainer,
+    e_a: EnumContainer,
+    e_b: EnumContainer,
+    e_c: EnumContainer,
+    e_d: EnumContainer,
+    t: TupleContainer,
+    s: StructContainer,
+}
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct PrimitiveTypes {
+    u8: u8,
+    u16: u16,
+    u32: u32,
+    u64: u64,
+    u128: u128,
+    usize: usize,
+    i8_max: i8,
+    i8_min: i8,
+    i16_max: i16,
+    i16_min: i16,
+    i32_max: i32,
+    i32_min: i32,
+    i64_max: i64,
+    i64_min: i64,
+    i128_max: i128,
+    i128_min: i128,
+    isize_max: isize,
+    isize_min: isize,
+    f32: f32,
+    f64: f64,
+    bool_true: bool,
+    bool_false: bool,
+    // TODO: char is not supported yet
+    // char: char,
+    none_zero: NonZero<u8>,
+}
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct CompoundTypes {
+    static_byte_slice: &'static [u8],
+    static_str: &'static str,
+    array: [u8; 10],
+    range: Range<u16>,
+    option_some: Option<u8>,
+    option_none: Option<u8>,
+    tuple: (u8, StructContainer, EnumContainer, TupleContainer),
+}
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct AllocTypes {
+    a: Vec<u8>,
+    b: String,
+    c: HashMap<u16, u8>,
+}
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct HeaplessTypes {
+    a: heapless::Vec<u8, 10>,
+    b: heapless::String<10>,
+    c: heapless::LinearMap<u8, u16, 10>,
+}
+
+#[derive(Debug, Serialize, PostcardBindings)]
+struct AllTests {
+    a: ContainerTypes,
+    b: PrimitiveTypes,
+    c: CompoundTypes,
+    d: AllocTypes,
+    e: HeaplessTypes,
+    f: e::E,
 }
 
 mod e {
     use super::*;
 
-    #[derive(Debug, Serialize, Deserialize, PostcardBindings)]
+    #[derive(Debug, Serialize, PostcardBindings)]
     pub struct E(pub u8, pub f::F);
 
     pub mod f {
         use super::*;
 
-        #[derive(Debug, Serialize, Deserialize, PostcardBindings)]
+        #[derive(Debug, Serialize, PostcardBindings)]
         pub struct F(pub u8);
     }
 }
@@ -64,7 +125,20 @@ fn main() {
             .runtime_type_checks(true)
             .esm_module(false)
             .module_structure(true),
-        generate_bindings!(A, B, e::E, C, D, e::f::F),
+        generate_bindings!(
+            UnionContainer,
+            StructContainer,
+            EnumContainer,
+            TupleContainer,
+            ContainerTypes,
+            PrimitiveTypes,
+            CompoundTypes,
+            AllocTypes,
+            HeaplessTypes,
+            AllTests,
+            e::E,
+            e::f::F
+        ),
     )
     .unwrap();
 
@@ -77,48 +151,141 @@ fn main() {
         python::GenerationSettings::enable_all()
             .runtime_type_checks(true)
             .module_structure(true),
-        generate_bindings!(A, B, e::E, C, D, e::f::F),
+        generate_bindings!(
+            UnionContainer,
+            StructContainer,
+            EnumContainer,
+            TupleContainer,
+            ContainerTypes,
+            PrimitiveTypes,
+            CompoundTypes,
+            AllocTypes,
+            HeaplessTypes,
+            AllTests,
+            e::E,
+            e::f::F
+        ),
     )
     .unwrap();
 
-    let d = D {
-        a: 123,
-        b: C::D {
-            a: vec![6, 123],
-            b: B(
-                231,
-                vec![182, 1234],
-                "hello from rust".into(),
-                HashMap::new(),
+    let all_tests = AllTests {
+        a: ContainerTypes {
+            e_a: EnumContainer::A,
+            e_b: EnumContainer::B(123),
+            e_c: EnumContainer::C(
+                123,
+                StructContainer {
+                    a: 123,
+                    b: 123,
+                    c: 123,
+                    d: 123,
+                },
             ),
-            c: false,
+            e_d: EnumContainer::D {
+                a: 123,
+                b: StructContainer {
+                    a: 123,
+                    b: 123,
+                    c: 123,
+                    d: 123,
+                },
+            },
+            s: StructContainer {
+                a: 123,
+                b: 123,
+                c: 123,
+                d: 123,
+            },
+            t: TupleContainer(
+                123,
+                StructContainer {
+                    a: 123,
+                    b: 123,
+                    c: 123,
+                    d: 123,
+                },
+                EnumContainer::A,
+            ),
+            u: UnionContainer,
         },
-        c: A,
-        d: vec![234, 21],
-        e: None,
-        f: &[123, 23],
-        g: "Hello",
-        h: (10..30),
-        i: {
-            let mut map = HashMap::new();
-            map.insert("a".into(), 23);
-            map
+        b: PrimitiveTypes {
+            u8: u8::MAX,
+            u16: u16::MAX,
+            u32: u32::MAX,
+            u64: u64::MAX,
+            u128: u128::MAX,
+            usize: usize::MAX,
+            i8_max: i8::MAX,
+            i8_min: i8::MIN,
+            i16_max: i16::MAX,
+            i16_min: i16::MIN,
+            i32_max: i32::MAX,
+            i32_min: i32::MIN,
+            i64_max: i64::MAX,
+            i64_min: i64::MIN,
+            i128_max: i128::MAX,
+            i128_min: i128::MIN,
+            isize_max: isize::MAX,
+            isize_min: isize::MIN,
+            f32: 123.123,
+            f64: 123.123,
+            bool_true: true,
+            bool_false: false,
+            // TODO: char is not supported yet
+            // char: 'a',
+            none_zero: NonZero::new(123).unwrap(),
         },
-        j: {
-            let mut map = HashMap::new();
-            map.insert(234, 23);
-            map.insert(23, 99);
-            map
+        c: CompoundTypes {
+            static_byte_slice: &[123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+            static_str: "Hello",
+            array: [123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+            range: 10..20,
+            option_some: Some(123),
+            option_none: None,
+            tuple: (
+                123,
+                StructContainer {
+                    a: 123,
+                    b: 123,
+                    c: 123,
+                    d: 123,
+                },
+                EnumContainer::A,
+                TupleContainer(
+                    123,
+                    StructContainer {
+                        a: 123,
+                        b: 123,
+                        c: 123,
+                        d: 123,
+                    },
+                    EnumContainer::A,
+                ),
+            ),
         },
-        k: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        l: e::E(123, e::f::F(234)),
-        m: (123, "hello".into(), vec![1, 2, 3]),
-        n: true,
-        o: NonZero::new(123).unwrap(),
-        p: -123,
-        q: -1234,
+        d: AllocTypes {
+            a: vec![123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+            b: "Hello".into(),
+            c: {
+                let mut map = HashMap::new();
+                map.insert(123, 123);
+                map
+            },
+        },
+        e: HeaplessTypes {
+            a: heapless::Vec::from_slice(&[123, 123, 123, 123, 123, 123, 123, 123, 123, 123])
+                .unwrap(),
+            b: heapless::String::from_str("Hello").unwrap(),
+            c: {
+                let mut map = heapless::LinearMap::new();
+                map.insert(123, 123).unwrap();
+                map
+            },
+        },
+        f: e::E(123, e::f::F(123)),
     };
-    let postcard_bytes = postcard::to_vec::<_, 100>(&d).unwrap();
+
+    let postcard_bytes = postcard::to_vec::<_, 300>(&all_tests).expect("Failed to serialize");
     let mut file =
         std::fs::File::create(std::env::current_dir().unwrap().join("serialized.bytes")).unwrap();
     file.write_all(postcard_bytes.as_slice()).unwrap();
--- postcard-bindgen/test_python_export.py
+++ postcard-bindgen/test_python_export.py
@@ -1,16 +1,79 @@
 from py_test_bindings import *
+from json import dumps
 
-
-d_own = D(a=23, b=C_B(32), c=A(), d=[43, 34, 23], e=None, f=[34, 23, 12], g="feerfef", h=range(3, 23), i={
-          "df": 434}, j={3: 23}, k=[23, 23, 12, 12, 1, 1, 1, 9, 1, 10], l=e.E(234, e.f.F(23)), m=(32, "def", [32, 232]), n=True, o=45, p=-23, q=-1234)
-
+all_tests = AllTests(
+    a=ContainerTypes(
+        u=UnionContainer(),
+        e_a=EnumContainer_A(),
+        e_b=EnumContainer_B(123),
+        e_c=EnumContainer_C(123, StructContainer(a=123, b=123, c=123, d=123)),
+        e_d=EnumContainer_D(a=123, b=StructContainer(
+            a=123, b=123, c=123, d=123)),
+        t=TupleContainer(123, StructContainer(
+            a=123, b=123, c=123, d=123), EnumContainer_A()),
+        s=StructContainer(a=123, b=123, c=123, d=123)
+    ),
+    b=PrimitiveTypes(
+        u8=255,
+        u16=65535,
+        u32=4294967295,
+        u64=18446744073709551615,
+        u128=340282366920938463463374607431768211455,
+        usize=18446744073709551615,
+        i8_max=127,
+        i8_min=-128,
+        i16_max=32767,
+        i16_min=-32768,
+        i32_max=2147483647,
+        i32_min=-2147483648,
+        i64_max=9223372036854775807,
+        i64_min=-9223372036854775808,
+        i128_max=170141183460469231731687303715884105727,
+        i128_min=-170141183460469231731687303715884105728,
+        isize_max=9223372036854775807,
+        isize_min=-9223372036854775808,
+        f32=123.123,
+        f64=123.123,
+        bool_true=True,
+        bool_false=False,
+        none_zero=123
+    ),
+    c=CompoundTypes(
+        static_byte_slice=[123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+        static_str="Hello",
+        array=[123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+        range=range(10, 20),
+        option_some=123,
+        option_none=None,
+        tuple=(
+            123,
+            StructContainer(a=123, b=123, c=123, d=123),
+            EnumContainer_A(),
+            TupleContainer(123, StructContainer(
+                a=123, b=123, c=123, d=123), EnumContainer_A())
+        )
+    ),
+    d=AllocTypes(
+        a=[123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+        b="Hello",
+        c={123: 123}
+    ),
+    e=HeaplessTypes(
+        a=[123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+        b="Hello",
+        c={123: 123}
+    ),
+    f=e.E(123, e.f.F(123))
+)
 
 ser = open("serialized.bytes", "rb").read()
-d = deserialize(D, ser)
+d = deserialize(AllTests, ser)
 print(d)
 
-ser_own = serialize(d_own)
+ser_own = serialize(all_tests)
 print(ser_own)
 
-d_des = deserialize(D, ser_own)
+d_des = deserialize(AllTests, ser_own)
 print(d_des)
+
+assert ser == ser_own
