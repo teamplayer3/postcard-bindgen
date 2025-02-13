609d018dfa6f7ed5cf0e053d885c856a20d81280
Alexander Hübener <alex.teamplayer@gmail.com>
2025-02-13 11:24:17 +0100

    fix example

--- postcard-bindgen/examples/small.rs
+++ postcard-bindgen/examples/small.rs
@@ -1,4 +1,4 @@
-use postcard_bindgen::{build_package, generate_bindings, PackageInfo, PostcardBindings};
+use postcard_bindgen::{generate_bindings, javascript, python, PackageInfo, PostcardBindings};
 use serde::Serialize;
 
 #[derive(Serialize, PostcardBindings)]
@@ -17,12 +17,29 @@ fn main() {
         .unwrap()
     );
 
-    build_package(
+    javascript::build_package(
         std::env::current_dir().unwrap().as_path(),
         PackageInfo {
             name: "test".into(),
             version: "0.1.0".try_into().unwrap(),
         },
+        javascript::GenerationSettings::enable_all()
+            .runtime_type_checks(true)
+            .esm_module(false)
+            .module_structure(true),
+        generate_bindings!(Test),
+    )
+    .unwrap();
+
+    python::build_package(
+        std::env::current_dir().unwrap().as_path(),
+        PackageInfo {
+            name: "test".into(),
+            version: "0.1.0".try_into().unwrap(),
+        },
+        python::GenerationSettings::enable_all()
+            .runtime_type_checks(true)
+            .module_structure(true),
         generate_bindings!(Test),
     )
     .unwrap();
